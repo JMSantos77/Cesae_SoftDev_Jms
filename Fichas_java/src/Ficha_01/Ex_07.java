ee7b8e491310a62a77f2c0d6004b167285f531f8
José Miguel Santos <jmcpsantos@gmail.com>
2024-02-19 14:16:15 +0000

    Exercício 7
    Commit Ex_07

---
+++ Fichas_java/src/Ficha_01/Ex_07.java
@@ -0,0 +1,27 @@
+package Ficha_01;
+
+import java.util.Scanner;
+
+public class Ex_07 {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        double valProdutoA, valProdutoB, valProdutoC, valorTotal, valorComDesconto;
+
+        System.out.println("Insira valor do Produto A:");
+        valProdutoA = input.nextDouble();
+
+        System.out.println("Insira valor do Produto B:");
+        valProdutoB = input.nextDouble();
+
+        System.out.println("Insira valor do Produto C:");
+        valProdutoC = input.nextDouble();
+
+        valorTotal = valProdutoA + valProdutoB + valProdutoC;
+
+        valorComDesconto = valorTotal - (valorTotal * 0.1);
+
+        System.out.println("O valor com desconto é: " + valorComDesconto);
+    }
+}
