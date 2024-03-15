49e73de23e3e430a98f44ddd2c6ff688981b2cad
JMSantos77 <1222636@isep.ipp.pt>
2024-03-15 22:10:51 +0000

    Changes to printwriter

--- Fichas_java/src/Trabalho_Grupo_1/printWriter.java
+++ Fichas_java/src/Trabalho_Grupo_1/printWriter.java
@@ -11,15 +11,17 @@ import java.io.PrintWriter;
 public class printWriter {
     public static void main(String[] args) throws FileNotFoundException {
 
-        String edit = "Bla Bla Bla";
+        String edit = "Bla Bla Bla!";
 
-         File testeEscrita = new File("C:/Users\\softdev\\Documents\\GitHub\\Cesae_SoftDev_Jms\\Fichas_java\\src\\Trabalho_Grupo_1\\testeEscrita.txt");
+         //File testeEscrita = new File("C:/Users\\softdev\\Documents\\GitHub\\Cesae_SoftDev_Jms\\Fichas_java\\src\\Trabalho_Grupo_1\\testeEscrita.txt");
+        File testeEscrita = new File("C:\\Users\\jmcps\\Cesae_SoftDev\\Cesae_SoftDev_Jms\\Fichas_java\\src\\Trabalho_Grupo_1\\testeEscrita.txt");
 
         PrintWriter printWriter = new PrintWriter(testeEscrita);
 
         printWriter.println(edit); // Passo o texto como argumento.
-        printWriter.println("A seguir ao Bla Bla Bla"); // Passo o texto directamente.
+        printWriter.println("A seguir ao Bla Bla Bla."); // Passo o texto directamente.
         printWriter.println("E agora 23?");
+        printWriter.println("Vai achar o file? Achou!");
 
         printWriter.close();
     }
