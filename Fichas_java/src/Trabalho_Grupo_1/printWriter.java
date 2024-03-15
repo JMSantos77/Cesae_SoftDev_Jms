ff894c87175b1cb4b53cb640048afc7419e77f92
José Miguel Santos <jmcpsantos@gmail.com>
2024-03-15 17:03:51 +0000

    Printwriter

---
+++ Fichas_java/src/Trabalho_Grupo_1/printWriter.java
@@ -0,0 +1,26 @@
+package Trabalho_Grupo_1;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+
+/**
+ * Teste de usar o PrintWriter.
+ */
+
+public class printWriter {
+    public static void main(String[] args) throws FileNotFoundException {
+
+        String edit = "Bla Bla Bla";
+
+         File testeEscrita = new File("C:/Users\\softdev\\Documents\\GitHub\\Cesae_SoftDev_Jms\\Fichas_java\\src\\Trabalho_Grupo_1\\testeEscrita.txt");
+
+        PrintWriter printWriter = new PrintWriter(testeEscrita);
+
+        printWriter.println(edit); // Passo o texto como argumento.
+        printWriter.println("A seguir ao Bla Bla Bla"); // Passo o texto directamente.
+        printWriter.println("E agora 23?");
+
+        printWriter.close();
+    }
+}
---
+++ Fichas_java/src/Trabalho_Grupo_1/teste.java
@@ -0,0 +1,24 @@
+package Trabalho_Grupo_1;
+
+/**
+ * Exemplo de Criar uma excepção com "try...catch".
+ */
+
+public class teste {
+    public static void main(String[] args) {
+
+        int numbers[] = {4, 8, 16, 32, 64, 120, 236};
+        int deno[] = {2, 0, 4, 4, 0, 2};
+
+        for (int i = 0; i < numbers.length; i++) {
+            try {
+                System.out.println(numbers[i] + "/" + deno[i] + " = " + numbers[i] / deno[i]);
+            } catch (ArithmeticException exc) {
+                System.out.println("Excep 1: Não se pode dividir por zero!");
+            } catch (ArrayIndexOutOfBoundsException exc) {
+                //Pq num tem tamanho maior que deno.
+                System.out.println("Excep 2: Não foi encontrado o elemento correspondente!");
+            }
+        }
+    }
+}
